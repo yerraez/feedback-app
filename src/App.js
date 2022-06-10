da1a7f62361e34bcd79e0726ab46733769d4e1fb
yerraez <erraez.yessenia@gmail.com>
2022-06-10 11:53:37 -0500

    "Don't work aplication after use Router and Route"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {motion, AnimatePresence} from 'framer-motion'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { v4 as uuidv4} from 'uuid'
 import { useState } from 'react'
 import Header from './components/Header'
@@ -6,6 +7,7 @@ import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
 
 function App(){
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,15 +23,15 @@ function App(){
     }
   }
   return (
-      <>
-
+      <Router>
       <Header />
       <div className='container'>
           <FeedbackForm handleAdd={addFeedback}/>
           <FeedbackStats feedback={feedback}/>
           <FeedbackList feedback = {feedback} handleDelete={deleteFeedback} />
+          <Route path='/about'>This is the aout route</Route>
       </div>
-      </>
+      </Router>
   )  
 }
 
