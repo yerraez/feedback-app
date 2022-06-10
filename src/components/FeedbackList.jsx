d260b0f733e98a8048673ee10eb43b757d4a886d
layessenia.erraez <layessenia.erraez@MacBook-Pro-de-layesseniaerraez.local>
2022-06-10 11:08:04 -0500

    Initial Load Feedback Form

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,45 @@
+import {motion, AnimatePresence} from 'framer-motion'
+import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem"
+function FeedbackList({feedback, handleDelete}) {
+    if(!feedback || feedback.leght === 0){
+        return <p>Not Feedback yet</p>
+    }
+    return (
+        <div claasname='feedback-list'>
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div 
+                    key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    >
+                    <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+                    </motion.div>
+                ))
+                }
+            </AnimatePresence>
+        </div>
+      )
+//   return (
+//     <div claasname='feedback-list'>
+//         {feedback.map((item) => (
+//             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+//         ))
+//         }
+//     </div>
+//   )
+}
+FeedbackList.propTypes ={
+    feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+
+    })
+    ),
+}
+
+export default FeedbackList
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,18 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'First comment feedback'
+      },
+      {
+        id: 2,
+        rating: 8,
+        text: 'Second comment feedback'
+      },
+      {
+        id: 9,
+        rating: 10,
+        text: 'Third comment feedback'
+      },
+]
+export default FeedbackData
