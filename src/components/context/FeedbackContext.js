aabd5f697765191b184f49b552a19d51f4671eb5
yerraez <erraez.yessenia@gmail.com>
2022-06-20 17:00:05 -0500

    delete and update API methods

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -38,16 +38,26 @@ export const FeedbackProvider = ({children}) => {
       }
 
       // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm('¿Estas seguro que deseas eliminar el comentario?'))
         {
-          setFeedback(feedback.filter((item) => item.id !== id))
+            await fetch(`/feedback/${id}`, { method: 'DELETE' })
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
       }
     
-    const updateFeedback = (id, upItem) => {
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`,{
+          method: 'PUT',
+          headers:  {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
         setFeedback(
-            feedback.map((item) => (item.id === id ? { ...item, ...upItem } : item)))
+            feedback.map((item) => (item.id === id ? { ...item, ...data } : item)))
     }
 
       //Set item to be updated
